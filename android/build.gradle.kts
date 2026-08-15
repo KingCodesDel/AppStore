ac6c715dd376d867a90d987fc160c4762a05ff91
KingCodesDel <olamideashaolu15@gmail.com>
2026-08-15 19:36:58 +0100

    dfav

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,8 +15,20 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+// Force every Android plugin/subproject to use compileSdk 36
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.library") || plugins.hasPlugin("com.android.application")) {
+            extensions.findByType(com.android.build.gradle.BaseExtension::class.java)?.apply {
+                compileSdkVersion(36)
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
